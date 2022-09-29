a15d7518b1b956317f8089d06cd06b0ebe62c237
Arthur Akira <arthurak10.at@gmail.com>
2022-09-29 20:12:12 -0300

    feat: add login cooperative controller and route

--- src/controllers/CooperativeControllers/index.ts
+++ src/controllers/CooperativeControllers/index.ts
@@ -2,6 +2,9 @@ import { CooperativeRepository } from "../../repositories/prisma/CooperativeRepo
 import { CreateCooperativeService } from "../../services/CooperativeServices/CreateCooperativeService";
 import { CryptUtils } from "../../utils/CryptUtils";
 import { CreateCooperativeController } from "./CreateCooperativeController";
+import { LoginCooperativeService } from "../../services/CooperativeServices/LoginCooperativeService";
+import { JWTUtils } from "../../utils/JWTUtils";
+import { LoginCooperativeController } from "./LoginCooperativeController";
 
 function getCooperativeRepo() {
   return new CooperativeRepository();
@@ -9,8 +12,9 @@ function getCooperativeRepo() {
 
 function getUtils() {
   const cryptUtils = new CryptUtils();
+  const jwtUtils = new JWTUtils(process.env.JWT_SECRET);
 
-  return { cryptUtils };
+  return { cryptUtils, jwtUtils };
 }
 
 export function createCooperativeController() {
@@ -18,6 +22,15 @@ export function createCooperativeController() {
   const { cryptUtils } = getUtils();
   const service = new CreateCooperativeService(repository, cryptUtils);
   const controller = new CreateCooperativeController(service);
+
+  return controller;
+}
+
+export function loginCooperativeController() {
+  const repository = getCooperativeRepo();
+  const { cryptUtils, jwtUtils } = getUtils();
+  const service = new LoginCooperativeService(repository, cryptUtils, jwtUtils);
+  const controller = new LoginCooperativeController(service);
 
   return controller;
 }
