51505633223a705809ac82d27e182ebc5fb3519c
Arthur Akira <arthurak10.at@gmail.com>
2022-10-03 20:57:38 -0300

    fix: remove cooperativeId from collection schema

--- src/schemas/collectionSchemas.ts
+++ src/schemas/collectionSchemas.ts
@@ -3,6 +3,5 @@ import { CollectionRequest } from "../@types/CollectionTypes";
 
 export const collectionSchema = Joi.object<CollectionRequest>({
   userId: Joi.string().required(),
-  cooperativeId: Joi.string().required(),
   types: Joi.array().min(1).required(),
 });
