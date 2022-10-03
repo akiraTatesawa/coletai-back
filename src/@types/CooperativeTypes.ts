7cf8e5456950a70b6aa6a94c6760f79446bdfb3c
Arthur Akira <arthurak10.at@gmail.com>
2022-10-03 18:46:30 -0300

    feat: add get all cooperatives location method in repository

--- src/@types/CooperativeTypes.ts
+++ src/@types/CooperativeTypes.ts
@@ -3,3 +3,8 @@ import { Cooperative } from "@prisma/client";
 export type CreateCooperativePrisma = Omit<Cooperative, "id" | "created_at">;
 
 export type LoginCooperative = Pick<Cooperative, "email" | "password">;
+
+export type CooperativeLocation = Pick<
+  Cooperative,
+  "id" | "latitude" | "longitude"
+>;
