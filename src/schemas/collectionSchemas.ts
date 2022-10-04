42507184bf2d45ae4124168d9e43a5c33d1afa60
Arthur Akira <arthurak10.at@gmail.com>
2022-10-04 11:26:56 -0300

    feat: add description key to collection schema

--- src/schemas/collectionSchemas.ts
+++ src/schemas/collectionSchemas.ts
@@ -2,5 +2,6 @@ import Joi from "joi";
 import { CollectionRequest } from "../@types/CollectionTypes";
 
 export const collectionSchema = Joi.object<CollectionRequest>({
+  description: Joi.string().max(140).required(),
   types: Joi.array().min(1).required(),
 });
