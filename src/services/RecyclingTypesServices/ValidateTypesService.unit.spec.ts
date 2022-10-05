54252a9bcbb610e00031c8eb5acb1508a6dd8d21
Arthur Akira <arthurak10.at@gmail.com>
2022-10-05 17:14:40 -0300

    test: add validate recycling types service unit test

---
+++ src/services/RecyclingTypesServices/ValidateTypesService.unit.spec.ts
@@ -0,0 +1,42 @@
+import { RecyclingTypes } from "@prisma/client";
+import { MockRecyclingTypesRepository } from "../../repositories/prisma/mocks/MockRecyclingTypesRepository";
+import {
+  ValidateTypesService,
+  IValidateTypesService,
+} from "./ValidateTypesService";
+import { IRecyclingTypesRepository } from "../../repositories/IRecyclingTypesRepository";
+import { RecyclingTypesFactory } from "../../../tests/factories/RecyclingTypesFactory";
+import { CustomError } from "../../entities/CustomError";
+
+describe("Validate Recycling Types Service", () => {
+  const repository: IRecyclingTypesRepository =
+    new MockRecyclingTypesRepository();
+  const service: IValidateTypesService = new ValidateTypesService(repository);
+  const recyclingTypesFactory = new RecyclingTypesFactory();
+
+  it("Should validate all the types without throwing an error", async () => {
+    const types: RecyclingTypes[] = recyclingTypesFactory.generateTypes();
+    const reqTypes = recyclingTypesFactory.generateValidType();
+
+    jest.spyOn(repository, "getAll").mockResolvedValueOnce(types);
+
+    await expect(service.execute(reqTypes)).resolves.not.toThrow();
+    expect(repository.getAll).toHaveBeenCalledTimes(1);
+  });
+
+  it("Should throw an error if there is an invalid type", async () => {
+    const types: RecyclingTypes[] = recyclingTypesFactory.generateTypes();
+    const invalidReqType = recyclingTypesFactory.generateInvalidType();
+
+    jest.spyOn(repository, "getAll").mockResolvedValueOnce(types);
+
+    await expect(service.execute(invalidReqType)).rejects.toEqual(
+      new CustomError(
+        "error_not_found",
+        `Recycling type "${invalidReqType[0].name}" not found`
+      )
+    );
+
+    expect(repository.getAll).toHaveBeenCalledTimes(1);
+  });
+});
